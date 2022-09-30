ada1bf47513aa0436aaf5f5fc1bdcf808f43bc13
bacnh7427 <bacnh7427@co-well.com.vn>
2022-09-30 10:06:29 +0700

    add function health, game over, core function, start new game

--- prj-monster/app.js
+++ prj-monster/app.js
@@ -6,21 +6,55 @@ const app = Vue.createApp({
         return {
             playerHealth: 100,
             monsterHealth: 100, 
-            currentRound: 0
+            currentRound: 0, 
+            winner: null
         };
     },
     computed: {
         monsterBarStyles(){
+            if(this.monsterHealth < 0){
+                return {width: '0%'}
+            }
             return { width: this.monsterHealth + '%' }
         },
         playerBarStyles(){
+            if(this.playerHealth < 0){
+                return {width: '0%'}
+            }
             return { width: this.playerHealth + '%' }
         }, 
         myUseSpecialAttack(){
             return this.currentRound % 3 !== 0
         }
     },
+    watch:{
+        playerHealth(value){
+            if(value <= 0 && this.monsterHealth <= 0){
+                // a draw
+                this.winner = 'draw';
+            } else if(value <= 0){
+                // player lost
+                this.winner = 'monster';
+            }
+        },
+        monsterHealth(value){
+            if(value <= 0 && this.playerHealth <= 0){
+                // a draw
+                this.winner = 'draw';
+            } else if( value <= 0){
+                // Monster lost
+                this.winner = 'player';
+            }
+        }
+    },
     methods: {
+        startGame(){
+            this.playerHealth = 100;
+            this.monsterHealth = 100;
+            this.winner = null;
+            this.currentRound = 0;
+            
+        },
         attackMonster(){
             this.currentRound++;
             const attackValue = getRandomValue(5, 12);
@@ -45,6 +79,10 @@ const app = Vue.createApp({
             } else {
                 this.playerHealth += healthValue
             }
+            this.attackPlayer();
+        },
+        surrender(){
+            this.winner = 'monster';
         }
     }
 });
